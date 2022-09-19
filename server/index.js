720c9093d840836a2a8f61618936a6b9431ca54b
saurabh singh <saurabh.9901s@gmail.com>
2022-09-19 16:10:07 +0530

    backend complete

--- server/index.js
+++ server/index.js
@@ -3,3 +3,15 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
 
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://sawcodes:qEvB5sZxg4MOLNEE@cluster0.lrik1zd.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
