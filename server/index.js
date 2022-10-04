25c8438686f0c35445f162eb9bc844635ca46909
saurabh singh <saurabh.9901s@gmail.com>
2022-10-04 19:33:49 +0530

    connected herokku

--- server/index.js
+++ server/index.js
@@ -14,9 +14,12 @@ const PORT = process.env.PORT || 4000;
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-app.use('/saurabh',postRoutes);
 
-//const CONNECTION_URL = 'mongodb+srv://sawcodes:qEvB5sZxg4MOLNEE@cluster0.lrik1zd.mongodb.net/?retryWrites=true&w=majority';
+app.use('/',postRoutes);
+
+app.get('/', (req,res) => {
+  res.send('hello to memories API');
+})
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
